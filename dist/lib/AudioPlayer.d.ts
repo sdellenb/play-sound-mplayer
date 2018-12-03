a13848fddbe0c09b386906365c957e6a36457d23
roland smolin <rgsmolin@gmail.com>
2018-12-03 16:53:13 +0100

    fix: export interface

--- dist/lib/AudioPlayer.d.ts
+++ dist/lib/AudioPlayer.d.ts
@@ -1,13 +1,1 @@
-export interface IAudioPlayer {
-    readonly isPlaying: boolean;
-    readonly isPaused: boolean;
-    readonly isMuted: boolean;
-    readonly currentVolume: number;
-    play(path: string, options?: any): void;
-    pause(): void;
-    resume(): void;
-    stop(): void;
-    mute(): void;
-    unMute(): void;
-    setVolume(value: number): void;
-}
+export {};
